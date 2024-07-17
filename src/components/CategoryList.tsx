039cd09ad419b0a99fd410aefa0329fb6746d5fd
VICTOR <95864890+Legolixx@users.noreply.github.com>
2024-07-17 16:50:29 -0300

    Update CategoryList.tsx

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const categoryList = () => {
   return (
     <div className="px-4 overflow-x-scroll scrollbar-hide">
-      <div className="flex gap-4 md:gap-8">
+      <div className="flex w-full items-center justify-center gap-4 md:gap-8">
         <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
           <div className="relative bg-slate-100 w-full h-96">
             <Image
