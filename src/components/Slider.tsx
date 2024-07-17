dd2a4dc2187aba27d1195edfbc05ce8394ae24c0
VICTOR <95864890+Legolixx@users.noreply.github.com>
2024-07-17 10:53:27 -0300

    Update Slider.tsx

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -53,7 +53,7 @@ const Slider = () => {
           </div>
         ))}
       </div>
-      <div className="absolute w-full left-1/2 bottom-6 md:bottom-8 flex gap-4">
+      <div className="absolute left-1/2 bottom-6 md:bottom-8 flex gap-4">
         {slides.map((slide, index) => (
           <div
             className={`w-3 h-3  rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
