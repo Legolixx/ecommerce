bcf010d6c56e82039e98794d1b79deb7fbcb7e84
VICTOR <95864890+Legolixx@users.noreply.github.com>
2024-07-17 16:44:29 -0300

    Update CategoryList.tsx

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -75,20 +75,6 @@ const categoryList = () => {
             Category Name
           </h1>
         </Link>
-        <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
-          <div className="relative bg-slate-100 w-full h-96">
-            <Image
-              src="https://images.pexels.com/photos/46710/pexels-photo-46710.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt="category_img"
-              fill
-              sizes="20vw"
-              className="object-cover rounded-md"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-clip tracking-wide">
-            Category Name
-          </h1>
-        </Link>
       </div>
     </div>
   );
