a9fa0ac94c90fc3d2c6f92e9b8eaf648d998f2cc
VICTOR <95864890+Legolixx@users.noreply.github.com>
2024-07-18 17:01:54 -0300

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,31 @@
 import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
+import { wixClientServer } from "@/lib/wixClientServer";
+import { Suspense } from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
+
   return (
     <div className="">
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl text-slate-900 font-semibold">Destaques</h1>
-        <ProductList />
+        <Suspense fallback={"loading"}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID as string}
+            limit={4}
+          />
+        </Suspense>
       </div>
       <div className="mt-24">
-        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12 text-slate-900 font-semibold">Categorias</h1>
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12 text-slate-900 font-semibold">
+          Categorias
+        </h1>
         <CategoryList />
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl text-slate-900 font-semibold">Novidades</h1>
-        
       </div>
     </div>
   );
